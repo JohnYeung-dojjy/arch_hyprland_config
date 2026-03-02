04f1cceab34428742d36244f7d678475f83e95de
John Yeung <cfyeung2357@gmail.com>
2026-03-02 11:01:19 -0800

    Add fish shell config, includes custom alias for cachyos

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+alias gst="git status"
+alias gl="git log --oneline --graph --decorate --all"
